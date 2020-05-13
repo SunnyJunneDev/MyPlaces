3a4b3393b23e988ec43b8d10b8db7725453f61a4
Svetlana Shardakova <svetlana.mikhaljova@gmail.com>
2020-05-13 17:40:19 +0300

    Custom cell class added

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Светлана Шардакова on 13.05.2020.
+//  Copyright © 2020 Светлана Шардакова. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var namePlace: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
